d606dbba8a0212f0c6afa83f899f24ac6d1bc08c
Brandon Davidson <brandondcpp@gmail.com>
2015-03-11 16:36:17 -0500

    changed achIcon_* to this_*

--- lua/starfall/libs_cl/panel/achievementicon.lua
+++ lua/starfall/libs_cl/panel/achievementicon.lua
@@ -1,25 +1,25 @@
 SF.Panel.AchievementIcon = {}
 
-local achIcon_methods, achIcon_metamethods = SF.Typedef( "Panel.AchievementIcon", SF.Panel.Panel.Metatable )
+local this_methods, this_metamethods = SF.Typedef( "Panel.AchievementIcon", SF.Panel.Panel.Metatable )
 
 local punwrap = SF.Panel.unwrap
 
 local function pwrap( object )
 	object = SF.Panel.wrap( object )
-	debug.setmetatable( object, achIcon_metamethods )
+	debug.setmetatable( object, this_metamethods )
 	return object
 end
 
 SF.Panel.AchievementIcon.wrap = pwrap
 SF.Panel.AchievementIcon.unwrap = punwrap
 
-SF.Panel.AchievementIcon.Methods = achIcon_methods
-SF.Panel.AchievementIcon.Metatable = achIcon_metamethods
+SF.Panel.AchievementIcon.Methods = this_methods
+SF.Panel.AchievementIcon.Metatable = this_metamethods
 
 --- Sets the achievement to be displayed by AchievementIcon
 -- @param id Achievement number ID
-function achIcon_methods:setAchievement( id )
-	SF.CheckType( self, achIcon_metamethods )
+function this_methods:setAchievement( id )
+	SF.CheckType( self, this_metamethods )
 	SF.CheckType( id, "number" )
 
 	if not SF.Permissions.check( SF.instance.player, self, "panel.access" ) then SF.throw( "Insufficient permissions", 2 ) return end
