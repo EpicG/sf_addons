28542f6880c5338bee9c42c8e5911348caaa45c4
Brandon Davidson <brandondcpp@gmail.com>
2015-03-11 16:20:50 -0500

    added AchievementIcon methods

--- lua/starfall/libs_cl/panel/achievementicon.lua
+++ lua/starfall/libs_cl/panel/achievementicon.lua
@@ -15,3 +15,14 @@ SF.Panel.AchievementIcon.unwrap = punwrap
 
 SF.Panel.AchievementIcon.Methods = achIcon_methods
 SF.Panel.AchievementIcon.Metatable = achIcon_metamethods
+
+--- Sets the achievement to be displayed by AchievementIcon
+-- @param id Achievement number ID
+function achIcon_methods:setAchievement( id )
+	SF.CheckType( self, achIcon_metamethods )
+	SF.CheckType( id, "number" )
+
+	if not SF.Permissions.check( SF.instance.player, self, "panel.access" ) then SF.throw( "Insufficient permissions", 2 ) return end
+
+	punwrap( self ):SetAchievement( id )
+end
