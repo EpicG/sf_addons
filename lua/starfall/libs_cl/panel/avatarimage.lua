b724ecd31a8d2ce26b3e95db40e9f3ae25c8966d
Brandon Davidson <brandondcpp@gmail.com>
2015-03-11 16:21:30 -0500

    added avatarimage methods

--- lua/starfall/libs_cl/panel/avatarimage.lua
+++ lua/starfall/libs_cl/panel/avatarimage.lua
@@ -15,3 +15,38 @@ SF.Panel.AvatarImage.unwrap = punwrap
 
 SF.Panel.AvatarImage.Methods = avatarImg_methods
 SF.Panel.AvatarImage.Metatable = avatarImg_metamethods
+
+local validSize = {
+	"16" = true,
+	"32" = true,
+	"64" = true,
+	"84" = true,
+	"128" = true,
+	"184" = true
+}
+
+--- Sets the avatar for the panel
+-- @param player The player to use as an avatar
+-- @param size the size of avatar to use
+-- Sizes can be: 16, 32, 64, 84, 128, and 184
+function avatarImg_methods:setPlayer( player, size )
+	SF.CheckType( self, avatarImg_metamethods )
+	SF.CheckType( player, SF.Types[ "Player" ] )
+	SF.CheckType( size, "number" )
+
+	if not validSize[ tostring( size ) ] then SF.throw( "Invalid panel size", 2 ) return end
+
+	punwrap( self ):SetPlayer( SF.UnwrapObject( player ), size )
+end
+
+--- Sets the avatar for the panel via SteamID
+-- @param id The 64bit SteamID
+-- @param size the size of avatar to use
+-- Sizes can be: 16, 32, 64, 84, 128, and 184
+function panel_methods:setSteamID( id, size )
+	SF.CheckType( self, panel_metamethods )
+	SF.CheckType( id, "string" )
+	SF.CheckType( size, "number" )
+
+	punwrap( self ):SetSteamID( id, size )
+end
