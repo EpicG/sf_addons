7de0278cdc35146593de169792a4286b7daaed60
Brandon Davidson <brandondcpp@gmail.com>
2015-03-13 22:24:12 -0500

    added ddrawer methods

--- lua/starfall/libs_cl/panel/ddrawer.lua
+++ lua/starfall/libs_cl/panel/ddrawer.lua
@@ -1,17 +1,38 @@
 SF.Panel.DDrawer = {}
 
-local ddrawer_methods, ddrawer_metamethods = SF.Typedef( "Panel.DDrawer", SF.Panel.Panel.Metatable )
+local this_methods, this_metamethods = SF.Typedef( "Panel.DDrawer", SF.Panel.Panel.Metatable )
 
 local punwrap = SF.Panel.unwrap
 
 local function pwrap( object )
 	object = SF.Panel.wrap( object )
-	debug.setmetatable( object, ddrawer_metamethods )
+	debug.setmetatable( object, this_metamethods )
 	return object
 end
 
 SF.Panel.DDrawer.wrap = pwrap
 SF.Panel.DDrawer.unwrap = punwrap
 
-SF.Panel.DDrawer.Methods = ddrawer_methods
-SF.Panel.DDrawer.Metatable = ddrawer_metamethods
+SF.Panel.DDrawer.Methods = this_methods
+SF.Panel.DDrawer.Metatable = this_metamethods
+
+--- Toggles the state of the drawer
+function this_methods:toggle( )
+	SF.CheckType( self, this_metamethods )
+
+	punwrap( self ):Toggle( )
+end
+
+--- Opens the drawer
+function this_methods:open( )
+	SF.CheckType( self, this_metamethods )
+
+	punwrap( self ):Open( )
+end
+
+--- Closes the drawer
+function this_methods:close( )
+	SF.CheckType( self, this_metamethods )
+
+	punwrap( self ):Close( )
+end
