b4749a332610843bb1f4dc8225a6bf65a043c783
Brandon Davidson <brandondcpp@gmail.com>
2015-03-13 23:42:59 -0500

    added dkillicon methods

--- lua/starfall/libs_cl/panel/dkillicon.lua
+++ lua/starfall/libs_cl/panel/dkillicon.lua
@@ -1,17 +1,41 @@
 SF.Panel.DKillIcon = {}
 
-local dkillIcon_methods, dkillIcon_metamethods = SF.Typedef( "Panel.DKillIcon", SF.Panel.Panel.Metatable )
+local this_methods, this_metamethods = SF.Typedef( "Panel.DKillIcon", SF.Panel.Panel.Metatable )
 
 local punwrap = SF.Panel.unwrap
 
 local function pwrap( object )
 	object = SF.Panel.wrap( object )
-	debug.setmetatable( object, dkillIcon_metamethods )
+	debug.setmetatable( object, this_metamethods )
 	return object
 end
 
 SF.Panel.DKillIcon.wrap = pwrap
 SF.Panel.DKillIcon.unwrap = punwrap
 
-SF.Panel.DKillIcon.Methods = dkillIcon_methods
-SF.Panel.DKillIcon.Metatable = dkillIcon_metamethods
+SF.Panel.DKillIcon.Methods = this_methods
+SF.Panel.DKillIcon.Metatable = this_metamethods
+
+--- Gets the name of the killicon
+--@return The name of the killicon
+function this_methods:getName( )
+	SF.CheckType( self, this_metamethods )
+
+	return punwrap( self ).m_Name
+end
+
+--- Sets the name of the killicon to use
+--@param killicon The name of the killicon
+function this_methods:setName( killicon )
+	SF.CheckType( self, this_metamethods )
+	SF.CheckType( killicon, "string" )
+
+	punwrap( self ).m_Name = killicon
+end
+
+--- Resizes the killicon to fit the icon being shown
+function this_methods:resizeToContents( )
+	SF.CheckType( self, this_metamethods )
+
+	punwrap( self ):ResizeToContents( )
+end
