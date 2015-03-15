928ab75735c54e7d18bf47a6bbc651e566a6d446
Brandon Davidson <brandondcpp@gmail.com>
2015-03-14 19:59:36 -0500

    added dalphabar methods

--- lua/starfall/libs_cl/panel/dalphabar.lua
+++ lua/starfall/libs_cl/panel/dalphabar.lua
@@ -1,17 +1,37 @@
 SF.Panel.DAlphaBar = {}
 
-local dalphaBar_methods, dalphaBar_metamethods = SF.Typedef( "Panel.DAlphaBar", SF.Panel.DPanel.Metatable )
+local this_methods, this_metamethods = SF.Typedef( "Panel.DAlphaBar", SF.Panel.DPanel.Metatable )
 
 local punwrap = SF.Panel.unwrap
 
 local function pwrap( object )
 	object = SF.Panel.wrap( object )
-	debug.setmetatable( object, dalphaBar_metamethods )
+	debug.setmetatable( object, this_metamethods )
 	return object
 end
 
 SF.Panel.DAlphaBar.wrap = pwrap
 SF.Panel.DAlphaBar.unwrap = punwrap
 
-SF.Panel.DAlphaBar.Methods = dalphaBar_methods
-SF.Panel.DAlphaBar.Metatable = dalphaBar_metamethods
+SF.Panel.DAlphaBar.Methods = this_methods
+SF.Panel.DAlphaBar.Metatable = this_metamethods
+
+function this_methods:setValue( value )
+	SF.CheckType( self, this_metamethods )
+	SF.CheckType( value, "number" )
+
+	punwrap( self ).m_Value = value
+end
+
+function this_methods:getValue( )
+	SF.CheckType( self, this_metamethods )
+
+	return punwrap( self ).m_Value
+end
+
+function this_methods:setBarColor( color )
+	SF.CheckType( self, this_metamethods )
+	SF.CheckType( color, SF.Types[ "Color" ] )
+
+	punwrap( self ).m_BarColor = SF.UnwrapObject( color )
+end
