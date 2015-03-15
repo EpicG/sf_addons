7b633e1353089c3aa2a7a479346c0f07aef2c774
Brandon Davidson <brandondcpp@gmail.com>
2015-03-14 20:04:58 -0500

    added dbubblecontainer methods

--- lua/starfall/libs_cl/panel/dbubblecontainer.lua
+++ lua/starfall/libs_cl/panel/dbubblecontainer.lua
@@ -1,17 +1,32 @@
 SF.Panel.DBubbleContainer = {}
 
-local dbubbleContainer_methods, dbubbleContainer_metamethods = SF.Typedef( "Panel.DBubbleContainer", SF.Panel.DPanel.Metatable )
+local this_methods, this_metamethods = SF.Typedef( "Panel.DBubbleContainer", SF.Panel.DPanel.Metatable )
 
 local punwrap = SF.Panel.unwrap
 
 local function pwrap( object )
 	object = SF.Panel.wrap( object )
-	debug.setmetatable( object, dbubbleContainer_metamethods )
+	debug.setmetatable( object, this_metamethods )
 	return object
 end
 
 SF.Panel.DBubbleContainer.wrap = pwrap
 SF.Panel.DBubbleContainer.unwrap = punwrap
 
-SF.Panel.DBubbleContainer.Methods = dbubbleContainer_methods
-SF.Panel.DBubbleContainer.Metatable = dbubbleContainer_metamethods
+SF.Panel.DBubbleContainer.Methods = this_methods
+SF.Panel.DBubbleContainer.Metatable = this_metamethods
+
+--- Sets the speech bubble position and size
+--@param x The x position of the dialog point
+--@param y The y position of the dialog point
+--@param w The width of the bubble container
+--@param h The height of the bubble container
+function this_methods:openForPos( x, y, w, h )
+	SF.CheckType( self, this_metamethods )
+	SF.CheckType( x, "number" )
+	SF.CheckType( y, "number" )
+	SF.CheckType( w, "number" )
+	SF.CheckType( h, "number" )
+
+	punwrap( self ):OpenForPos( x, y, w, h )
+end
