3db4658eb68f3c3054b9528b87e9c0514d3986e8
Brandon Davidson <brandondcpp@gmail.com>
2015-03-14 22:45:58 -0500

    added some ddragbase methods

--- lua/starfall/libs_cl/panel/ddragbase.lua
+++ lua/starfall/libs_cl/panel/ddragbase.lua
@@ -1,17 +1,50 @@
 SF.Panel.DDragBase = {}
 
-local ddragBase_methods, ddragBase_metamethods = SF.Typedef( "Panel.DDragBase", SF.Panel.DPanel.Metatable )
+local this_methods, this_metamethods = SF.Typedef( "Panel.DDragBase", SF.Panel.DPanel.Metatable )
 
 local punwrap = SF.Panel.unwrap
 
 local function pwrap( object )
 	object = SF.Panel.wrap( object )
-	debug.setmetatable( object, ddragBase_metamethods )
+	debug.setmetatable( object, this_metamethods )
 	return object
 end
 
 SF.Panel.DDragBase.wrap = pwrap
 SF.Panel.DDragBase.unwrap = punwrap
 
-SF.Panel.DDragBase.Methods = ddragBase_methods
-SF.Panel.DDragBase.Metatable = ddragBase_metamethods
+SF.Panel.DDragBase.Methods = this_methods
+SF.Panel.DDragBase.Metatable = this_metamethods
+
+function this_methods:setDnD( name )
+	SF.CheckType( self, this_metamethods )
+	SF.CheckType( name, "string" )
+
+	punwrap( self ).m_DNDName = name
+end
+
+function this_methods:getDnD( )
+	SF.CheckType( self, this_metamethods )
+
+	return punwrap( self ).m_DNDName
+end
+
+function this_methods:setUseLiveDrag( enable )
+	SF.CheckType( self, this_metamethods )
+	SF.CheckType( enable, "boolean" )
+
+	punwrap( self ).m_bLiveDrag = enable
+end
+
+function this_methods:getUseLiveDrag( )
+	SF.CheckType( self, this_metamethods )
+
+	return punwrap( self ).m_bLiveDrag
+end
+
+function this_methods:setDropPos( pos )
+	SF.CheckType( self, this_metamethods )
+	SF.CheckType( pos, "string" )
+
+	punwrap( self ):SetDropPos( pos )
+end
